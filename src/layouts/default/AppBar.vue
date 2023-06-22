5f38b1acdc38b074d0d36e0c9c11ed9c1a4d4b53
Eliezir <eliezir2004@gmail.com>
2023-06-22 01:17:14 -0300

    configurando o router

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app-bar 
-  color="deep-purple-accent-2"
+  color="green-accent-2"
   >
     <v-app-bar-title>
       <span class="title">Vue Grocery Store</span>
     </v-app-bar-title>
       <v-tabs>
-        <v-tab>Home</v-tab>
-        <v-tab>Products</v-tab>
-        <v-tab>Cart</v-tab>
+        <v-tab to="/">Home</v-tab>
+        <v-tab >Products</v-tab>
+        <v-tab to="/carrinho"> Cart </v-tab>
       </v-tabs>
   
   </v-app-bar>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
         name: 'Home',
         component: () => import('@/views/Home.vue'),
       },
+      {
+        path: '/carrinho',
+        name: 'Carrinho',
+        component: () => import('@/views/CarrinhoCompras.vue'),
+      },
     ],
   },
 ]

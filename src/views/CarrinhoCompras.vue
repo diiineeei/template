5a709f7401dff104cadf880df16e2fd95aa6d935
Eliezir <eliezir2004@gmail.com>
2023-06-22 01:17:30 -0300

    view do carrinho de compras

---
+++ src/views/CarrinhoCompras.vue
@@ -0,0 +1,10 @@
+<template>
+     <AppBar/>
+    <CarrinhoCompras/>
+  </template>
+  
+  <script setup>
+import AppBar from '@/layouts/default/AppBar.vue';
+import CarrinhoCompras from '@/components/CarrinhoCompras.vue'
+  </script>
+  

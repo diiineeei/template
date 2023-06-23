68357d2cabe03ff3ac10973bffdd0edfa4a5a1af
Eliezir <eliezir2004@gmail.com>
2023-06-22 23:07:55 -0300

    navbar responsiva

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,21 +1,57 @@
 <template>
-  <v-app-bar 
-  color="green-accent-2"
-  >
+  <v-app-bar color="green-accent-2" >
     <v-app-bar-title>
       <span class="title">Vue Grocery Store</span>
     </v-app-bar-title>
-      <v-tabs>
-        <v-tab to="/">Home</v-tab>
-        <v-tab >Products</v-tab>
-        <v-tab to="/carrinho"> Cart </v-tab>
-      </v-tabs>
-  
+    <v-tabs class="d-none d-sm-flex">
+      <v-tab v-for="page in pages" :key="page.title" :to="page.path">
+      {{ page.title}}
+      </v-tab>
+    </v-tabs>
+
+    <v-app-bar-nav-icon class="d-flex d-sm-none" size="70" @click.stop="drawer = !drawer">
+    </v-app-bar-nav-icon>
   </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        location="top"
+        temporary
+        class="bg-green-accent-2"
+      >
+      <v-list variant="elevated" >
+         <v-list-item v-for="page in pages" :key="page.title" :to="page.path" class="bg-green-accent-2" elevation="0">
+          <v-list-item-title class="font-weight-bold text-uppercase">
+            {{ page.title }}</v-list-item-title>
+        </v-list-item>
+</v-list>
+      </v-navigation-drawer>
+
+
 </template>
 
 <script setup>
-  //
+import {ref} from 'vue'
+const pages = [
+  {
+    title: "home",
+    path:"/",
+},
+{
+  title:"Products",
+  path:"/",
+},
+{
+  title:"Cart",
+  path:"/carrinho",
+},
+{
+  title:"Login",
+  path:"/login",
+},
+
+]
+const drawer = ref(false)
+
 </script>
 
 

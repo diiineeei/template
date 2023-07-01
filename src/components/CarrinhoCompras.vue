25834e75941d41a1e448f309b0ef829e08b10027
Eliezir <eliezir2004@gmail.com>
2023-07-01 02:59:08 -0300

    tela carrinho, time line e grid

--- src/components/CarrinhoCompras.vue
+++ src/components/CarrinhoCompras.vue
@@ -1,101 +1,39 @@
 <template>
-    <v-container>
-
-        <v-row>
-
-            <v-col cols="12" class="mx-0 pa-0">
-                <v-list lines="one" class="bg-transparent pa-4" transition="fade-transition" elevation="3">
-                    <h2 class="text-h4 text-center">
-                        <v-badge color="success" :content="store.productsCar.length">
-                            <v-icon icon="mdi-cart" size="small"></v-icon>
-                        </v-badge>
-                        SHOPPING CART
-                    </h2>
-                    <v-list-item v-for="produto in store.productsCar" :key="produto.name" color="primary" rounded="sm"
-                        class="my-5">
-                        <v-container class="item-main-container pa-0">
-                            <div class="mr-5 w-md-25" >
-                            <v-list-item-title>{{ produto.name }}</v-list-item-title>
-                            <v-list-item-subtitle>{{ productPrice(produto) }}</v-list-item-subtitle>
-                            </div>
-                            <v-btn variant="text" icon="mdi-plus-circle" color="green-accent-4" 
-                            @click="changeItemQuantity(produto,'add')"
-                            ></v-btn>
-                            {{ produto.quantity }}
-                            <v-btn variant="text" icon="mdi-minus-circle" color="red-accent-2" 
-                            @click="changeItemQuantity(produto,'remove')"
-                            ></v-btn>
-                        </v-container>
-                        <template v-slot:prepend>
-                            <v-avatar size="85" rounded="0">
-                                <v-img :src="produto.img"></v-img>
-                            </v-avatar>
-                        </template>
-                        <template v-slot:append>
-                            <v-hover v-slot="{ isHovering, props }">
-                                <v-btn v-bind="props" :icon="isHovering ? 'mdi-delete-empty' : 'mdi-delete'" variant="text"
-                                    elevation="0" color="red-darken-4" 
-                                    @click="removeItem(produto)"
-                                    />
-                            </v-hover>
-                        </template>
-                    </v-list-item>
-                    <v-row>
-                    <v-col class="d-flex justify-space-around">
-                    <h2 class="text-center">Total: {{ precoTotal }}</h2>
-                    <v-btn append-icon="mdi-shopping-outline" color="success" >Order Now</v-btn>
-                    </v-col>
-                    </v-row>
-                </v-list>
-            </v-col>
-        </v-row>
-
-
-    </v-container>
+<v-container id="container" fluid>
+    <v-row class="d-flex justify-space-between">
+        <v-col id="timeline-container" cols="12" class="d-none d-md-flex">
+            <v-timeline direction="horizontal" line-inset="40"  truncate-line="both">
+                <v-timeline-item dot-color="white" size="x-large" :icon-color="timeline == 0 ? 'green-accent-2':'grey-lighten-3'" fill-dot icon="mdi-cart"></v-timeline-item>
+                <v-timeline-item dot-color="white" size="x-large" :icon-color="timeline == 1 ? 'green-accent-2':'grey-lighten-3'" fill-dot icon="mdi-account"></v-timeline-item>
+                <v-timeline-item dot-color="white" size="x-large" :icon-color="timeline == 2 ? 'green-accent-2':'grey-lighten-3'" fill-dot icon="mdi-credit-card-outline"></v-timeline-item>
+                <v-timeline-item dot-color="white" size="x-large" :icon-color="timeline == 3 ? 'green-accent-2':'grey-lighten-3'" fill-dot icon="mdi-check-circle"></v-timeline-item>
+            </v-timeline>
+        </v-col>
+        <v-col id="carrinho-container" class="bg-grey-lighten-3 pa-10 rounded" cols="12" md="8">
+            <v-row>
+                <v-col cols="12" class="d-flex justify-space-between">
+                    <p class="text-h5 font-weight-bold text-center d-flex align-self-end"><v-icon size="x-small" color="green-accent-2" class="px-5 align-self-center">mdi-basket</v-icon>PRODUTOS</p>
+                    <v-btn prepend-icon="mdi-delete" variant="outlined" color="red">REMOVER TODOS PRODUTOS</v-btn>
+                </v-col>
+    
+                <v-divider></v-divider>
+            </v-row>
+        </v-col>
+        <v-col id="total-container" class="bg-grey-lighten-3" cols="12" md="3"></v-col>
+    </v-row>
+</v-container>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import {produtosAppStore} from '@/store/app'
-const store = produtosAppStore()
+import {ref} from 'vue';
 
-const precoTotal = computed(() => {
-    const value = store.productsCar.reduce((total, produtoAtual) => total + produtoAtual.price * produtoAtual.quantity, 0);
-    return value.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    })
-})
-
-const productPrice = ((produto) => {
-    const value = produto.price * produto.quantity
-    return value.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    })
-})
-
-/* const props = defineProps({
-    produtos: Array
-})
- */
-
- const changeItemQuantity = ((produto, type)=>{
-    if(type === "remove" && store.productsCar[store.productsCar.indexOf(produto)].quantity == 1) {
-        removeItem(produto)
-    }
-    store.productsCar[store.productsCar.indexOf(produto)].quantity += type == "add" ? 1 : -1
- })
-
- const removeItem = ((produto)=>{
-    store.productsCar.splice( store.productsCar.indexOf(produto), 1)
- })
+const timeline = ref(0)
 
 </script>
 
+
 <style scoped>
-.item-main-container{
-    display: flex;
-    align-items: center;
+#container{
+    width: 80%;
 }
 </style>
